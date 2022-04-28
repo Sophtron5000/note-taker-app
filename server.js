c434ed79aa1cc9623cd5c516dcb2da691605bbb7
Sophia C <95388964+Sophtron5000@user.noreply.github.com>
2022-04-27 21:16:24 -0700

    add server.js & route to both HTML docs

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const api = require('./public/assets/js/index');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
