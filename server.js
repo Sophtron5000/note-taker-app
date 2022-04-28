a00d316f193d0e847ce93f630566240de3320b7b
Sophia C <95388964+Sophtron5000@user.noreply.github.com>
2022-04-27 22:37:51 -0700

    add route to landing page

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const api = require('./public/assets/js/index');
-
-const PORT = process.env.PORT || 3001;
+const path = require('path')
+//const api = require('./public/assets/js/index');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
